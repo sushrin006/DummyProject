bebe73c08ba1ed3e13f77898c108028b4f0806b2
krijal7@gmail.com <krijalshrestha7@gmail.com>
2026-06-06 21:02:50 +0545

    changes made

--- client/src/pages/ProductPage.tsx
+++ client/src/pages/ProductPage.tsx
@@ -7,20 +7,27 @@ export default function Products() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const fetchProducts = async (): Promise<void> => { // defines async function that fetches product
-      const res = await fetch("/api/products"); // sends request to server
-      const data: Product[] = await res.json();
-      setProducts(data.products); 
-    };
-
-    fetchProducts();
+    (async () => {
+      const res = await fetch("/api/products");
+      console.log("Status:", res.status);
+      console.log("OK?:", res.ok); 
+      if (!res.ok) {
+        const raw = await res.text();
+        console.error("Server returned error:", raw);
+        return;
+      }
+      const data = await res.json();
+      console.log("Data received:", data);
+      setProducts(data.products);
+    })();
   }, []);
 
-  return (
-    <div className="max-w-6xl mx-auto p-6">
-      <h1 className="text-3xl font-bold mb-6">Products</h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+  return (
+    <div className="max-w-7xl mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-6">Products</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+
         {products.map((p) => (
           <div
             key={p.id}
@@ -28,16 +35,11 @@ export default function Products() {
             className="border p-4 rounded shadow cursor-pointer hover:shadow-lg"
           >
             <h2 className="font-bold text-lg">{p.name}</h2>
-
-            <p className="text-gray-600 mt-2">
-              {p.description}
-            </p>
-
-            <p className="text-sm text-blue-500 mt-2">
-              {p.refNo}
-            </p>
+            <p className="text-gray-600 mt-2">{p.description}</p>
+            <p className="text-sm text-blue-500 mt-2">{p.refNo}</p>
           </div>
         ))}
+
       </div>
     </div>
   );
